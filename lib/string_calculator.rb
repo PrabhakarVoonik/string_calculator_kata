599f93e7a14cf8783b8c346468c6f4c435737e24
prabhakaran <mpkguru@gmail.com>
2025-08-11 15:05:55 +0530

    Changes for handling negative numbers in the given numbers string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -6,8 +6,14 @@ class StringCalculator
       delimiter_line, numbers_line = numbers.split("\n", 2) # split exactly into two portions.
       delimiter = delimiter_line[2] # 3rd character is the actual delimiter
       numbers_line.split(delimiter).map(&:to_i).sum # splitting by delimiter & find sum
+      nums = numbers_line.split(delimiter).map(&:to_i)
     else
-      numbers.split(/[,\n]/).map(&:to_i).sum
+      nums = numbers.split(/[,\n]/).map(&:to_i)
     end
+    
+    negatives = nums.select { |n| n < 0 }
+    raise "negative numbers not allowed #{negatives.join(',')}" unless negatives.empty?
+    
+    nums.sum
   end
 end
