3d8f016a15104d4eb612759055c38afb625d6d84
prabhakaran <mpkguru@gmail.com>
2025-08-11 14:59:49 +0530

    Adding Support for customer delimiter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,12 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
     
-    numbers.split(/[,\n]/).map(&:to_i).sum
+    if numbers.start_with?('//')
+      delimiter_line, numbers_line = numbers.split("\n", 2) # split exactly into two portions.
+      delimiter = delimiter_line[2] # 3rd character is the actual delimiter
+      numbers_line.split(delimiter).map(&:to_i).sum # splitting by delimiter & find sum
+    else
+      numbers.split(/[,\n]/).map(&:to_i).sum
+    end
   end
 end
