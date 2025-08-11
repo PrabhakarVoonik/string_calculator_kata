7dd485311fd9c6422ea7014b62ef66f2f25664d5
prabhakaran <mpkguru@gmail.com>
2025-08-11 16:20:44 +0530

    Handling more than one delimiter with multi character

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -5,11 +5,14 @@ class StringCalculator
     if numbers.start_with?('//')
       delimiter_line, numbers_line = numbers.split("\n", 2) # split exactly into two portions.
       if delimiter_line.start_with?('//[') && delimiter_line.end_with?(']') # check for multi char delimiter
-        delimiter = delimiter_line[3..-2] # delimiter is from 4th character to n-1 character 
+        delimiters = delimiter_line.scan(/\[([^\]]+)\]/).flatten # capturing delimiters inside [] 
+        normalized_string = numbers_line
+        delimiters.each { |delim| normalized_string = normalized_string.gsub(delim, ',') }
+        nums = normalized_string.split(',').map(&:to_i)
       else
         delimiter = delimiter_line[2]
+        nums = numbers_line.split(delimiter).map(&:to_i) # splitting by delimiter & changing to integer
       end
-      nums = numbers_line.split(delimiter).map(&:to_i) # splitting by delimiter & changing to integer
     else
       nums = numbers.split(/[,\n]/).map(&:to_i)
     end
