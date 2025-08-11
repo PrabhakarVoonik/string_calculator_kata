474ab3e1eda29c60a23339acb74ed53855ea8013
prabhakaran <mpkguru@gmail.com>
2025-08-11 15:46:19 +0530

    Adding support for multi char delimiter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,9 +4,12 @@ class StringCalculator
     
     if numbers.start_with?('//')
       delimiter_line, numbers_line = numbers.split("\n", 2) # split exactly into two portions.
-      delimiter = delimiter_line[2] # 3rd character is the actual delimiter
-      numbers_line.split(delimiter).map(&:to_i).sum # splitting by delimiter & find sum
-      nums = numbers_line.split(delimiter).map(&:to_i)
+      if delimiter_line.start_with?('//[') && delimiter_line.end_with?(']') # check for multi char delimiter
+        delimiter = delimiter_line[3..-2] # delimiter is from 4th character to n-1 character 
+      else
+        delimiter = delimiter_line[2]
+      end
+      nums = numbers_line.split(delimiter).map(&:to_i) # splitting by delimiter & changing to integer
     else
       nums = numbers.split(/[,\n]/).map(&:to_i)
     end
